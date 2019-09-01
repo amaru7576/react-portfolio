92df713380f039406e601c26e2d0226e7cb5bc3f
Alex Almendarez <hermeregildo.almendarez.com>
2019-08-31 21:30:00 -0500

    Initial commit

---
+++ src/components/app2.22.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import monment from 'moment';
+export default class App extends Component {
+    render() {
+        return ( <
+            div className = 'app' >
+            <
+            h1 > Alex Almendarez Portfollio < /h1> <
+            /div>
+        );
+    }
+}
