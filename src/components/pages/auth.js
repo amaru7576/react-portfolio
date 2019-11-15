621cd7dd66fa070b39d092a6031afe11d4a1ec05
Alex Almendarez <hermeregildo.almendarez.com>
2019-11-15 07:31:54 -0600

    sidebar

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import Login from "../auth/login";
-import loginImg from "../../../static/assets/images/auth/Login.jpg";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
 
 export default class Auth extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
         this.handleUnsuccessfulAuth = this.handleUnsuccessfulAuth.bind(this);
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,7 +4,8 @@ import PortfolioContainer from "../portfolio/portfolio-container";
 
 export default function() {
     return (
-        <div> 
+        <div>
+            <h2>Homepage</h2> 
             <PortfolioContainer />
         </div>
     );
