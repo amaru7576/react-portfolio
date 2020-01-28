f3b49536e9e94e1309e256d2c961417290ab1b8a
Alex Almendarez <hermeregildo.almendarez.com>
2020-01-28 07:00:41 -0600

    sign icon

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -293,7 +293,7 @@ export default class PortfolioForm extends Component {
     return (
       
         <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
-          <div>
+          <div className="two-column">
             <input
               type="text"
               name="name"
@@ -311,7 +311,7 @@ export default class PortfolioForm extends Component {
             />
           </div>
 
-          <div>
+          <div className="two-column">
             <input
               type="text"
               name="position"
@@ -324,6 +324,7 @@ export default class PortfolioForm extends Component {
               name="category"
               value={this.state.category}
               onChange={this.handleChange}
+              className="select-element"
             >
               <option value="eCommerce">eCommerce</option>
               <option value="Scheduling">Scheduling</option>
@@ -331,7 +332,7 @@ export default class PortfolioForm extends Component {
             </select>
           </div>
 
-          <div>
+          <div className="one-column">
             <textarea
               type="text"
               name="description"
@@ -347,25 +348,33 @@ export default class PortfolioForm extends Component {
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
               eventHandlers={this.handleThumbDrop()}
-            />
+            >
+              <div className="dz-message">Thumbnail</div>
+            </DropzoneComponent>
+
+
 
             <DropzoneComponent
               ref={this.bannerRef}
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
               eventHandlers={this.handleBannerDrop()}
-            />
+            >
+              <div className="dz-message">Banner</div>
+              </DropzoneComponent>
 
             <DropzoneComponent
               ref={this.logoRef}
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
               eventHandlers={this.handleLogoDrop()}
-            />
+            >
+              <div className="dz-message">Logo</div>
+              </DropzoneComponent>
           </div>
 
           <div>
-            <button type="submit">Save</button>
+            <button className="btn" type="submit">Save</button>
           </div>
         </form>
     );
--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,49 +1,58 @@
-// import React from "react";
-
-// const PortfolioSidebarList = props => {
-//     const portfolioList = props.data.map(portfolioItem => {
-//         return (
-//             <div key={portfolioItem.id} className="portfolio-item-thumb">
-//                 <div className="portfolio-thumb-img">
-//                     <img src={portfolioItem.thumb_image_url} />                       
-//                 </div>        
-//                 <h1 className="title">{portfolioItem.name}</h1>
-//                 <h2>{portfolioItem.id}</h2>    
-//             </div>
-//         );
-//     });
-
-//     return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
-// };
-
-// export default PortfolioSidebarList;
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const PortfolioSidebarList = props => {
-  const portfolioList = props.data.map(portfolioItem => {
-    return (
-      <div key={portfolioItem.id} className="portfolio-item-thumb">
-        <div className="portfolio-thumb-img">
-          <img src={portfolioItem.thumb_image_url} />
-        </div>
+    const portfolioList = props.data.map(portfolioItem => {
+        return (
+            <div key={portfolioItem.id} className="portfolio-item-thumb">
+                <div className="portfolio-thumb-img">
+                    <img src={portfolioItem.thumb_image_url} />                       
+                </div>    
 
-        <div className="text-content">
-          <div className="title">{portfolioItem.name}</div>
+                <div className="text-content">    
+                    <div className="title">{portfolioItem.name}</div>
+                    <a 
+                        className="delete-icon"
+                        onClick={() => props.handleDeleteClick(portfolioList)}
+                    >
+                        <FontAwesomeIcon icon="trash" />
+                    </a>
+                </div>    
+            </div>
+        );
+    });
 
-          <div className="actions">
-
-            <a className="action-icon" onClick={() => props.handleEditClick(portfolioItem)}>
-            </a>
-
-            <a className="action-icon" onClick={() => props.handleDeleteClick(portfolioItem)}>
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  });
-
-  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
+    return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 };
 
 export default PortfolioSidebarList;
+// import React from "react";
+
+// const PortfolioSidebarList = props => {
+//   const portfolioList = props.data.map(portfolioItem => {
+//     return (
+//       <div key={portfolioItem.id} className="portfolio-item-thumb">
+//         <div className="portfolio-thumb-img">
+//           <img src={portfolioItem.thumb_image_url} />
+//         </div>
+
+//         <div className="text-content">
+//           <div className="title">{portfolioItem.name}</div>
+
+//           <div className="actions">
+
+//             <a className="action-icon" onClick={() => props.handleEditClick(portfolioItem)}>
+//             </a>
+
+//             <a className="action-icon" onClick={() => props.handleDeleteClick(portfolioItem)}>
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   });
+
+//   return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>
+// };
+
+// export default PortfolioSidebarList;
