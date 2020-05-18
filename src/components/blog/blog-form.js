98f33d6a9c717f3361d27d8c27ff56a57b9a59a9
Alex Almendarez <hermeregildo.almendarez.com>
2020-05-17 21:22:47 -0500

    modaltype

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
 
 export default class BlogForm extends Component {
+    constructor(props) {
+        super(porps);
+
+        this.state = {
+            title: "",
+            blog_status: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })    
+    }
+
     render() {
         return (
             <form>
-                <input type="text" />
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={this.handleChange}
+                    name="title"
+                    placeholder="Blog Title"
+                    value={this.state.title}
+                />
+                
+                <input 
+                    type="text"
+                    onChange={this.handleChange}
+                    name="blog_status"
+                    placeholder="Blog Status"
+                    value={this.state.blog_status}
+                />
 
                 <button>Save</button>
             </form>
